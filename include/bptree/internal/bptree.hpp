a362a43af49478e1e2f792b7135b5a4f5f142a7c
Chi-En Wu <jason2506.mail@gmail.com>
2017-06-10 17:08:27 +0800

    :sparkles: Add `internal::bptree<>::bptree(key_compare)`

--- include/bptree/internal/bptree.hpp
+++ include/bptree/internal/bptree.hpp
@@ -39,10 +39,58 @@ class bptree {
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
 
+    class value_compare;
+
  public:  // Public Method(s)
+    bptree();
+    explicit bptree(key_compare const& comp);
+
     bool empty() const noexcept;
     size_type size() const noexcept;
+
+    key_compare key_comp() const;
+    value_compare value_comp() const;
+
+ private:  // Private Property(ies)
+    value_compare comp_;
 };
+
+/************************************************
+ * Declaration: class bptree::value_compare
+ ************************************************/
+
+template <typename K, typename T, std::size_t D, std::size_t N, typename C>
+class bptree<K, T, D, N, C>::value_compare {
+    friend bptree<K, T, D, N, C>;
+
+ public:  // Public Method(s)
+    value_compare(key_compare comp)
+      : comp_(comp)
+        { /* do nothing */ }
+
+    bool operator()(value_type const& lhs, value_type const& rhs) {
+        return comp_(lhs.first, rhs.first);
+    }
+
+ private:  // Private Property(ies)
+    key_compare comp_;
+};
+
+/************************************************
+ * Implementation: class bptree
+ ************************************************/
+
+template <typename K, typename T, std::size_t D, std::size_t N, typename C>
+bptree<K, T, D, N, C>::bptree()
+  : bptree(key_compare()) {
+      // do nothing
+}
+
+template <typename K, typename T, std::size_t D, std::size_t N, typename C>
+bptree<K, T, D, N, C>::bptree(key_compare const& comp)
+  : comp_(comp) {
+    // do nothing
+}
 
 template <typename K, typename T, std::size_t D, std::size_t N, typename C>
 bool bptree<K, T, D, N, C>::empty() const noexcept {
@@ -53,6 +101,16 @@ template <typename K, typename T, std::size_t D, std::size_t N, typename C>
 typename bptree<K, T, D, N, C>::size_type
 bptree<K, T, D, N, C>::size() const noexcept {
     return 0;
+}
+
+template <typename K, typename T, std::size_t D, std::size_t N, typename C>
+typename bptree<K, T, D, N, C>::key_compare bptree<K, T, D, N, C>::key_comp() const {
+    return comp_.comp_;
+}
+
+template <typename K, typename T, std::size_t D, std::size_t N, typename C>
+typename bptree<K, T, D, N, C>::value_compare bptree<K, T, D, N, C>::value_comp() const {
+    return comp_;
 }
 
 }  // namespace internal
--- test/bptree_test.cpp
+++ test/bptree_test.cpp
@@ -6,6 +6,8 @@
  *  Distributed under MIT License
  ************************************************/
 
+#include <utility>
+
 #include <gtest/gtest.h>
 
 #include <bptree/internal/bptree.hpp>
@@ -20,4 +22,25 @@ TEST(BPTreeTest, EmptyTree) {
 
     EXPECT_TRUE(tree.empty());
     EXPECT_EQ(0, tree.size());
+}
+
+TEST(BPTreeTest, ConstructTreeWithComp) {
+    using key = std::pair<double, char>;
+    auto compare = [](key const& lhs, key const& rhs) {
+        return lhs.first < rhs.first;
+    };
+
+    using bptree = bptree<key, int, degree, num_slots, decltype(compare)>;
+    bptree tree(compare);
+
+    bptree::key_compare key_comp = tree.key_comp();
+    bptree::value_compare value_comp = tree.value_comp();
+
+    bptree::value_type v1({{1.0, 'c'}, 3});
+    bptree::value_type v2({{-3.5, 'a'}, 8});
+
+    EXPECT_FALSE(key_comp(v1.first, v2.first));
+    EXPECT_TRUE(key_comp(v2.first, v1.first));
+    EXPECT_FALSE(value_comp(v1, v2));
+    EXPECT_TRUE(value_comp(v2, v1));
 }
